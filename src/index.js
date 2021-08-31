17007dc6b3a329c40d3966e9f6234742b2fa85ff
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-08-31 23:36:49 +0100

    feat: Edit profile.js file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/_main.scss';
 import './styles/_mision.scss';
+import './styles/_profile.scss';
 import './styles/_rockets.scss';
 import store from './redux/configureStore';
 
